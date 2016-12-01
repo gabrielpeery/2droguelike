19f058b260695dbff0e4de4d3cff72595d99a0fd
Gabriel Peery <Gabriel Peery>
2016-12-01 06:46:16 -0800

    started wall script crap

--- Assets/Scripts/BoardManager.cs
+++ Assets/Scripts/BoardManager.cs
@@ -1,8 +1,35 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
+using Random = UnityEngine.random;
 
 public class BoardManager : MonoBehaviour {
+
+	[Serializable]
+	public class Count {
+		public int minimum;
+		public int maximum
+
+		public Count (int min, int max) {
+			minimum = min;
+			maximum = max;
+		}
+	}
+
+	public int columns = 8;
+	public int rows = 8;
+	public Count wallCount = new Count (5,9);
+	public Count foodCount = new Count (1,5);
+	public GameObject exit;
+	public GameObject[] floorTiles;
+	public GameObject[] wallTiles;
+	public GameObject[] foodTiles;
+	public GameObject[] EnemyTiles;
+	public GameObject[] outWallTiles;
+
+	private Transform boardHolder;
+
 
 	// Use this for initialization
 	void Start () {
